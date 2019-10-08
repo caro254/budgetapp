314d9ffee9788dc768c7cc8cb5cb2c50fb3090ed
Carol254 <paulomugoo@gmail.com>
2019-10-08 14:18:28 +0300

    all the changes made to origin  master

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfdoo2ZOFAJ-9PlggiKg3x9fAV_ytFTwc",
+  authDomain: "budget-43b49.firebaseapp.com",
+  databaseURL: "https://budget-43b49.firebaseio.com",
+  projectId: "budget-43b49",
+  storageBucket: "",
+  messagingSenderId: "1023758493335",
+  appId: "1:1023758493335:web:e41553a955bdd8d3622bd2",
+  measurementId: "G-C9C4RQY08K"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
