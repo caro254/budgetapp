5f3df5d8ac626549375b9805aaff104e5285be06
Carol254 <paulomugoo@gmail.com>
2019-10-09 09:02:51 +0300

    all modified files origin master

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,4 +15,63 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set("very strange program");
+database.ref("expenses").on("value", snapshot => {
+  const expenses = [];
+  snapshot.forEach(childSnapshot => {
+    expenses.push({
+      id: childSnapshot.key,
+      ...childSnapshot.val()
+    });
+  });
+  console.log(expenses);
+});
+// database.ref("expenses").push({
+//   description: "Rent",
+//   note: "",
+//   amount: 109500,
+//   createdAt: 976123498763
+// });
+// database.ref("expenses").push({
+//   description: "Phone bill",
+//   note: "",
+//   amount: 5900,
+//   createdAt: 976123498763
+// });
+// database.ref("expenses").push({
+//   description: "Food",
+//   note: "",
+//   amount: 1200,
+//   createdAt: 976123498763
+// });
+
+// database.ref().on("value", snapshot => {
+//   const val = snapshot.val();
+//   console.log(`${val.name} is a  ${val.job.title} at ${val.job.company}`);
+// });
+
+// database
+//   .ref()
+//   .set({
+//     name: "Arturo",
+//     age: 29,
+//     stressLevel: 6,
+//     job: {
+//       title: "Software developer",
+//       company: "Google"
+//     },
+//     location: {
+//       city: "Rome",
+//       country: "Spain"
+//     }
+//   })
+//   .then(() => {
+//     console.log("Data is served");
+//   })
+//.catch(e => {
+//console.log("This failed, e");
+//});
+// database.ref().update({
+//   stressLevel: 9,
+//   "job/company": "Amazon",
+//   "location/city": "Barcelona"
+// });
