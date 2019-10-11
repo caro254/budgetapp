0e3edc2e298825bb36928ed277f843c7beb4d2aa
Carol254 <paulomugoo@gmail.com>
2019-10-11 16:59:01 +0300

    all mofidied files origin master

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
+const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-if (process.env.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === "test") {
   require("dotenv").config({ path: ".env.test" });
 } else if (process.env.NODE_ENV === "development") {
   require("dotenv").config({ path: ".env.development" });
@@ -75,9 +76,9 @@ module.exports = env => {
 
     devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
-      contentBase: path.join(__dirname, "public", "dist"),
+      contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
-      public: "/dist/"
+      publicPath: "/dist/"
     }
   };
 };
