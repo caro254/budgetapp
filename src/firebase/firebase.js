cfb05a67aceea971c2afd198428e7de243db5cf7
Carol254 <paulomugoo@gmail.com>
2019-10-11 12:12:35 +0300

    all modified files origin master

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,13 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyAfdoo2ZOFAJ-9PlggiKg3x9fAV_ytFTwc",
-  authDomain: "budget-43b49.firebaseapp.com",
-  databaseURL: "https://budget-43b49.firebaseio.com",
-  projectId: "budget-43b49",
-  storageBucket: "",
-  messagingSenderId: "1023758493335",
-  appId: "1:1023758493335:web:e41553a955bdd8d3622bd2",
-  measurementId: "G-C9C4RQY08K"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 firebase.initializeApp(config);
